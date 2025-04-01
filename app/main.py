979a801501e5ee2ccdbc57453d3e5163374f8973
paulinamarkiewicz641 <paulinamarkiewicz4506@gmail.com>
2025-04-01 11:52:16 +0200

    Class Person

--- app/main.py
+++ app/main.py
@@ -1,18 +1,16 @@
 class Person:
     people = {}
-    def __int__(self, name: string, age: int):
+    def __init__(self, name: str, age: int):
         self.name = name
         self.age = age
         self.wife = None
         self.husband = None
         Person.people[name] = self
-    pass
 
 @staticmethod
 def create_person_list(people: list) -> list:
     persons = [Person(person_dict["name"], person_dict["age"]) for person_dict in people]
         
-
     for person_dict in people:
         person = Person.people[person_dict["name"]]
         spouse_key = "wife" if "wife" in person_dict else "husband"
