de87e2f2a9b351de5ac46aa6aa7ce008a6c7a645
paulinamarkiewicz641 <paulinamarkiewicz4506@gmail.com>
2025-04-01 12:12:52 +0200

    Check again

--- app/main.py
+++ app/main.py
@@ -9,12 +9,18 @@ class Person:
 
 @staticmethod
 def create_person_list(people: list) -> list:
-    persons = [Person(person_dict["name"], person_dict["age"]) for person_dict in people]
-        
+    persons = [
+        Person(person_dict["name"], person_dict["age"]) 
+        for person_dict in people
+        ]
+
     for person_dict in people:
         person = Person.people[person_dict["name"]]
         spouse_key = person_dict.get("wife") or person_dict.get("husband")
         if spouse_key:
-            setattr(person, "wife" if "wife" in person_dict else "husband", Person.people[spouse_key])
-        
+            setattr(
+                person, "wife" if "wife" in person_dict else "husband", 
+                Person.people[spouse_key]
+                )
+    
     return persons
